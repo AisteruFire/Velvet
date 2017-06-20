c8321c2029bed54f4a2d7a430666e8de44a12353
Timothée Andres <andres.timothee@gmail.com>
2017-06-21 01:02:38 +0200

    Ajout du bouton lewd
    En attente d'une image satisfaisante pour le bouton...

--- scripts/beforeSubmit.js
+++ scripts/beforeSubmit.js
@@ -11,6 +11,35 @@ chrome.storage.sync.get({
 }, function(data){
     // ---------- Lewd button ----------
 
+    if (data.lewdTags)
+    {
+        var lewdButton = document.createElement("button");
+        lewdButton.setAttribute("title", "Lewd that for me please >//<");
+        lewdButton.setAttribute("class", "header__search__button");
+        lewdButton.setAttribute("type", "submit");
+        lewdButton.addEventListener("click", function(){
+            var field = document.getElementById("q");
+
+            if (document.getElementById("q").value)
+                document.getElementById("q").value += " AND (" + data.lewdTags.trim() + ")";
+            else
+                document.getElementById("q").value = data.lewdTags.trim();
+        });
+
+            var i = document.createElement("i");
+            i.setAttribute("style", "font-weight: bold; font-style: normal; font-size: 130%");
+            i.innerHTML = "L";
+            lewdButton.appendChild(i);
+
+            //Waiting for an appropriate icon
+            /*
+            var image = document.createElement("img");
+            image.setAttribute("style", "height: 16px;");
+            image.src = chrome.runtime.getURL("../icons/lewdIcon.png");
+            lewdButton.appendChild(image);
+            */
+        document.querySelector("form.header__search").appendChild(lewdButton);
+    }
 
     // ---------- Parsing the aliases ----------
     if (data.aliases)
